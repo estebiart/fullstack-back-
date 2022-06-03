47b554f44c1fb9d1107b7ce90f5042e9ce1e6d46
unknown <andersonlozada@quadi.co>
2022-06-02 22:16:51 -0500

    pipeline

--- test_persistencia.py
+++ test_persistencia.py
@@ -1,20 +1,16 @@
+"""
+Pruebas Persistencia
+"""
 import persistencia
-with open("pedidos.txt", "w", encoding="utf-8") as file:
- file.write("")
- file.close()
-
-pedidos = [{"nombre": "Pedro", "apellidos": "Gil de Diego"},{"nombre": "Michael", "apellidos": "Jordan"}]
-con=0
-
-for pedido in pedidos:
-        for nombre in pedido:
-             valor = pedido.get(nombre)
-
-             if(con==1):
-                 con=0
-                 persistencia.guardar_pedido(valor1,valor)
-                 print("{}, {} ".format( valor1,valor))
-             else:
-                valor1= valor
-                con= con+1
-           
+def test_guardar_pedido():
+ """
+ Prueba general
+ """
+ with open("pedidos.txt", "w+", encoding="utf-8") as file:
+    persistencia.guardar_pedido("Pedro", "Gil de Diego")
+    persistencia.guardar_pedido("Michael", "Jordan")
+    firstline = file.readline()
+    secondline = file.readline()
+    file.close()
+ assert firstline == "-Pedro Gil de Diego\n"
+ assert secondline == "-Michael Jordan\n"
