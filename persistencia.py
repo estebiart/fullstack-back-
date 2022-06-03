57924dd2370f5138663df2db7fb06f7fe4b0df19
unknown <andersonlozada@quadi.co>
2022-06-02 23:06:02 -0500

    pytest

--- persistencia.py
+++ persistencia.py
@@ -4,6 +4,6 @@ Persistencia.
 def guardar_pedido(nombre,apellido):
     """guardar."""
     with open("pedidos.txt", "a", encoding="utf-8") as file:
-        file.write(nombre + " " + apellido + "\n")
+        file.write("-" +nombre + " " + apellido + "\n")
         file.close()
     print("escritura exitosa")
